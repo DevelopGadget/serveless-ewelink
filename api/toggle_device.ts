237fb720e10183d2dd5db666d786b1ae4f3fca8a
Fernando Araujo <developgadget@gmail.com>
2021-01-09 10:55:42 -0500

    toggle normal

--- api/toggle_device.ts
+++ api/toggle_device.ts
@@ -1,6 +1,15 @@
-import { NowRequest, NowResponse } from '@vercel/node'
+import { NowRequest, NowResponse } from '@vercel/node';
+import EweLink from 'ewelink-api';
 
-export default (request: NowRequest, response: NowResponse) => {
-  const { name = 'World' } = request.query
-  response.status(200).send(`Hello ${name}!`)
+export default async (request: NowRequest, response: NowResponse) => {
+
+  const connection = new EweLink({
+    email: request.body.email,
+    password: request.body.password,
+  });
+
+  const status = await connection.toggleDevice(request.body.deviceid);
+
+  return response.send(status);
+
 }
